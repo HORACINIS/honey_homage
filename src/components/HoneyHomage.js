b0c11e0e95c00a121bc54d6aba60ea7cfc0a4727
horacinis <horacinis@gmail.com>
2021-01-25 20:16:31 +1100

    Removed all unnecessary files created by default and changed port to :8080

---
+++ src/components/HoneyHomage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CanBeDeleted from './CanBeDeleted';
+
+function HoneyHomage() {
+  return (
+    <React.Fragment>
+      <div className='container'>
+        <CanBeDeleted />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default HoneyHomage;
