013f95c37ba38aa8b546230c4edc55037e36da7e
horacinis <horacinis@gmail.com>
2021-01-28 01:04:37 +1100

    destructured products, makes it easier to read

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,25 +6,25 @@ const Products = () => {
     return (
         <div className="container">
             <div className="row">
-                {products && products.map(item => (
-                    <div className="col-sm-4 " key={item.id}>
-                        <div className="card" width="18rem;">
-                            <img src={item.url} className="card-img-top" alt="honey" />
-                            <div className="card-body">
-                                <h5 className="card-title">{item.title}</h5>
-                                <p className="card-text">{item.details}</p>
-
-                                <h5>${item.price} {item.onSale && <span className="p-1 mb-2 bg-danger text-white">ON SALE</span>}</h5>
-                                
-
-                                <a href="#!" className="btn btn-warning ">
-                                    <i style={{ fontSize: '2em' }} className="fas fa-plus-circle" />
+                {products && products.map(item => {
+                    const { id, url, title, details, price, onSale } = item;
+                    return (
+                        <div className="col-sm-4 " key={id}>
+                            <div className="card" width="18rem;">
+                                <img src={url} className="card-img-top" alt="honey" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{title}</h5>
+                                    <p className="card-text">{details}</p>
+                                    <h5>${price} {onSale && <span className="p-1 mb-2 bg-danger text-white">ON SALE</span>}</h5>
+                                    <a href="#!" className="btn btn-warning ">
+                                        <i style={{ fontSize: '2em' }} className="fas fa-plus-circle" />
                                     Add to cart
                                 </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    );
+                })}
             </div>
         </div>
     )
