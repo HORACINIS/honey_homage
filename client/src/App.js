e07bb6e2c5e14693bc66ea530ed70cc997661095
horacinis <horacinis@gmail.com>
2021-02-03 10:44:50 +1100

    Installed Router and removed unnecesary old components, HoneyHomenage and Checkout

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Switch, Route } from 'react-router-dom';
 import "./App.css";
 import HeroCover from "./components/HeroCover/HeroCover.js";
 import Navbar from "./components/Navbar/Navbar.js";
