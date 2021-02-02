57d15fb9508cb89c6e05ea3d535d057d712b1fba
Soni-247 <sonifisi@Gmail.com>
2021-02-02 12:19:02 +1100

    AboutUs page information text

--- client/src/components/AboutUs/AboutUs.js
+++ client/src/components/AboutUs/AboutUs.js
@@ -9,7 +9,9 @@ const AboutUs = () => {
                 <div className="AboutUsImageContainer"></div>
                 <div className="AboutUsTextContainer">
                     <h1>Our Story</h1>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita saepe rem facere laborum dolorem deleniti nihil libero fuga! Suscipit, asperiores nemo soluta officiis quam iure delectus ad debitis, culpa dolorem, error cum. Id similique eum tempora dolorum incidunt amet porro. Quod amet itaque accusantium.</p>
+                    <p>Before the outbreak of COVID-19, Australia had minimal time to react after taking a tragic blow of uncontrollable bushfires. Such destruction was vast, consuming both wildlife and rural homes. Affecting many farmers and crops, the road to recovery for our honey farm was steep after losing countless bees.</p>
+                    <h2>Our Battle</h2>
+                    <p>We are ready now! Our loss of beehives has been replenished with great patience, and ready for open. Honey Homage is our next step moving forward. The foundation of our brand focuses on quality and customer satisfaction. As we follow through, we continue to care for our bees and hope to share their golden, sweetness with the rest of Australia.</p>
                 </div>
             </div>
         </section>
