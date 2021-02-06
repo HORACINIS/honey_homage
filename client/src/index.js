93911a1dc2ddf01438af244c540e5e835be7c552
Andrew Bui <andrew.bui.au@gmail.com>
2021-02-07 00:22:32 +1100

    initialised testimonials page

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
 
