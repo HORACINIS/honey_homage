4db6a7cb43bdfa754739bd2a21b4cb9a069ddd0a
Soni-247 <sonifisi@Gmail.com>
2021-02-07 14:01:42 +1100

    Post requst. Need to define user

--- client/src/components/Products/AddToCart.js
+++ client/src/components/Products/AddToCart.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Products from './Products';
 import Quantity from './Quantity';
+import {useAuth0} from '@auth0/auth0-react'
 
 const AddToCart = (item) => {
+    const [Qty, setQty] = useState({Qty: ''});
+    const UserId =  user.id;
+    const Title = item.title;
+    const Price = item.price;
+    
 
     const addItem = () => {
-        console.log(Qty, item)
+        axios.post('http:localhost:3000/', {cart_id : {UserId}, product: {Title}, price: {Price}, quantity: {Qty}})
+    .then((response) => {console.log(response);},
+    (error) => {console.log(error);
+    });
     }
 
     return (
         <div>
-            <input type='number' name='Qty' placeholder='0' min='0' max='25'></input>
+            <input type='number' name='qty' placeholder='0' min='0' max='25' onChange={(e) => {setQty ({Qty: e.target.value })}}></input>
             <button onClick={addItem}>Add To Cart</button>
         </div>
     );
