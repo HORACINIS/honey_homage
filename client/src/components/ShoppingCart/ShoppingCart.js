86a342929d10d902c813294205ec87f1733f7abc
Andrew Bui <andrew.bui.au@gmail.com>
2021-02-08 12:03:53 +1100

    Restyled ShoppingCart and Added Order Total Logic

--- client/src/components/ShoppingCart/ShoppingCart.js
+++ client/src/components/ShoppingCart/ShoppingCart.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import './style.css';
 import honeyType01 from '../../images/ProductImages/honeyType01.jpg';
 import honeyType02 from '../../images/ProductImages/honeyType02.jpg';
 import honeyType03 from '../../images/ProductImages/honeyType03.jpg';
@@ -12,27 +13,40 @@ import honeyType09 from '../../images/ProductImages/honeyType09.jpg';
 const ShoppingCart = ({ pickedItems }) => {
     console.log(pickedItems)
 
+    let orderTotal = 0;
+
     return (
         <div>
             <h1>Shopping Cart</h1>
             {!pickedItems.length > 0 && <h2>No items in cart</h2>}
 
-            <ul>
+            <div className="shoppingCartItemSection">
                 {pickedItems.map((item, index) => {
                     const { _id, title, image, quantity, details, price } = item
                     const totalPerItem = quantity * price;
+                    
+                    orderTotal += totalPerItem;
                     return (
-                        <li key={_id}>
-                            <img src={honeyType01} alt="honey product"></img>
-                            <h2>{title}</h2>
-                            <p>{details}</p>
-                            <p>${price} ea</p>
-                            <h5>Quantity: {quantity} units</h5>
-                            <p><b>${totalPerItem}0</b></p>
-                        </li>
+                        <div key={_id} className="shoppingCartItemContainer">
+                            <div>
+                                <img src={honeyType01} alt="honey product" className="shoppingCartItemImage"></img>
+                            </div>
+                            <div>
+                                <h5>{title}</h5>
+                                <p>{details}</p>
+                            </div>
+                            <div>
+                                <h5>Quantity: {quantity} units</h5>
+                                <p>Total: ${totalPerItem}0</p>
+                            </div>
+                        </div>
                     )
                 })}
-            </ul>
+            </div>
+            <div>
+                <h3>Order Total: ${orderTotal}</h3>
+                <button>Confirm Checkout</button>
+            </div>
 
         </div>
     )
