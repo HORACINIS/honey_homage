ee4b729d8db6ceac8386c466eb25b1b64ec608b9
Andrew Bui <andrew.bui.au@gmail.com>
2021-02-10 01:58:46 +1100

    updated order output layout

--- client/src/pages/AdminPortal/AdminPortal.js
+++ client/src/pages/AdminPortal/AdminPortal.js
@@ -21,63 +21,71 @@ const AdminPortal = () => {
   }
 
   return (
-    <div>
-      <h1>Admin Portal</h1>
-
-      <div className="orderOutputContainer">
+    <div className="orderOutputContainer">
+      <h1>HoneyHUD</h1>
+      <div>
         {orders && orders.map((order, index) => {
           const { _id, comments, shippingAddress, contactNumber, status, items, total } = order;
           let randOrderNum = Math.floor(Math.random() * 1000000) + 1;
           return (
-            <div key={index}>
-              <p>UserID: {_id}</p>
-              <p>Order Number: {++index}-{randOrderNum} </p>
-              <p>Ph: {contactNumber}</p>
-              <p>Shipping Address: {shippingAddress}</p>
-              <p>Status: {status}</p>
-              <div>
-                {items.map((item, index) => {
-                  const { title, details, quantity, price, image } = item;
-                  // Should we add images? if not (we can remove image, in line 31 and 35)
+            <div className="orderOutputElementContainer">
+              <div key={index}>
+                <div className="orderNumberAndUserIDContainer">
+                  <p className="orderNumberAndUserIDContainerOrderNumberElement">Order Number: {++index}-{randOrderNum} </p>
+                  <p className="orderNumberAndUserIDContainerUserIDElement">UserID: {_id}</p>
+                </div>
 
-                  let orderImage = "";
+                <div className="orderItemsDisplaySectionParent">
+                  {items.map((item, index) => {
+                    const { title, details, quantity, price, image } = item;
+                    // Should we add images? if not (we can remove image, in line 31 and 35)
 
-                  if (image === "honeyType01") {
-                      orderImage = honeyType01;
-                  } else if (image === "honeyType02") {
-                      orderImage = honeyType02;
-                  } else if (image === "honeyType03") {
-                      orderImage = honeyType03;
-                  } else if (image === "honeyType04") {
-                      orderImage = honeyType04;
-                  } else if (image === "honeyType05") {
-                      orderImage = honeyType05;
-                  } else if (image === "honeyType06") {
-                      orderImage = honeyType06;
-                  } else if (image === "honeyType07") {
-                      orderImage = honeyType07;
-                  } else if (image === "honeyType08") {
-                      orderImage = honeyType08;
-                  } else if (image === "honeyType09") {
-                      orderImage = honeyType09;
-                  } 
+                    let orderImage = "";
 
-                  return (
-                    <div>
-                      <img src={orderImage} />
-                      <p>{title}..... x{quantity} ${price}</p>
-                    </div>
-                  );
-                })}
+                    if (image === "honeyType01") {
+                        orderImage = honeyType01;
+                    } else if (image === "honeyType02") {
+                        orderImage = honeyType02;
+                    } else if (image === "honeyType03") {
+                        orderImage = honeyType03;
+                    } else if (image === "honeyType04") {
+                        orderImage = honeyType04;
+                    } else if (image === "honeyType05") {
+                        orderImage = honeyType05;
+                    } else if (image === "honeyType06") {
+                        orderImage = honeyType06;
+                    } else if (image === "honeyType07") {
+                        orderImage = honeyType07;
+                    } else if (image === "honeyType08") {
+                        orderImage = honeyType08;
+                    } else if (image === "honeyType09") {
+                        orderImage = honeyType09;
+                    } 
+
+                    return (
+                      <div>
+                        <div className="orderItemsDisplaySection">
+                          <div className="orderItemsDisplayContainer">
+                            <img src={orderImage} className="orderItemsDisplayImage" />
+                            <p className="orderItemsDisplayInfo" >x{quantity} {title} | ${price * quantity}</p>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+                <p>Contact Number: {contactNumber}</p>
+                <p>Shipping Address: {shippingAddress}</p>
+                <p>Status: {status}</p>
+                <p>Total amount paid: ${total.toFixed(2)}</p>
+                <p>Comments: {comments}</p>
               </div>
-              <p>Total amount paid: ${total.toFixed(2)}</p>
-              <p>Comments: {comments}</p>
             </div>
           );
         })}
       </div>
 
-      <button onClick={shopOrders}>Show Orders</button>
+      <button className="orderDisplayButton" onClick={shopOrders}>Show Orders</button>
     </div>
   );
 };
